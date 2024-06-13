e6a9db1d1d6896cacf8c31786a5d017eea4518eb
Hrishikesh Thombare <hrishithombare15@gmail.com>
2024-06-13 23:32:26 +0530

    Koyeb Env Support

--- terabox.py
+++ terabox.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from pyrogram.enums import ChatMemberStatus
 from dotenv import load_dotenv
 from os import environ
+import os
 import time
 from status import format_progress_bar
 from video import download_video, upload_video
@@ -14,28 +15,28 @@ load_dotenv('config.env', override=True)
 
 logging.basicConfig(level=logging.INFO)
 
-api_id = environ.get('TELEGRAM_API', '')
+api_id = os.environ.get('TELEGRAM_API', '')
 if len(api_id) == 0:
     logging.error("TELEGRAM_API variable is missing! Exiting now")
     exit(1)
 
-api_hash = environ.get('TELEGRAM_HASH', '')
+api_hash = os.environ.get('TELEGRAM_HASH', '')
 if len(api_hash) == 0:
     logging.error("TELEGRAM_HASH variable is missing! Exiting now")
     exit(1)
     
-bot_token = environ.get('BOT_TOKEN', '')
+bot_token = os.environ.get('BOT_TOKEN', '')
 if len(bot_token) == 0:
     logging.error("BOT_TOKEN variable is missing! Exiting now")
     exit(1)
-dump_id = environ.get('DUMP_CHAT_ID', '')
+dump_id = os.environ.get('DUMP_CHAT_ID', '')
 if len(dump_id) == 0:
     logging.error("DUMP_CHAT_ID variable is missing! Exiting now")
     exit(1)
 else:
     dump_id = int(dump_id)
 
-fsub_id = environ.get('FSUB_ID', '')
+fsub_id = os.environ.get('FSUB_ID', '')
 if len(fsub_id) == 0:
     logging.error("FSUB_ID variable is missing! Exiting now")
     exit(1)
