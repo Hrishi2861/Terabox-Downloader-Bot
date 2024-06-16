a599dc78e76c32ed601d3c9b2daf04cab66d5d69
Hrishikesh Thombare <hrishithombare15@gmail.com>
2024-06-16 22:53:28 +0530

    Minor Change

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-aria2c --enable-rpc --rpc-listen-all=false --rpc-allow-origin-all --daemon && python terabox.py
+aria2c --enable-rpc --rpc-listen-all=false --rpc-allow-origin-all --daemon && python3 terabox.py
