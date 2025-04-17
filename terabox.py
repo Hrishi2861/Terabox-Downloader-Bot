d1a9c30668e9347f53bbf1f97ed5c4dd8979e3ba
Hrishikesh <hrishithombare15@gmail.com>
2025-04-17 14:13:38 +0530

    Minor

--- terabox.py
+++ terabox.py
@@ -515,7 +515,7 @@ flask_app = Flask(__name__)
 
 @flask_app.route('/')
 def home():
-    return "Bot is running"
+    return render_template("index.html")
 
 def run_flask():
     flask_app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
